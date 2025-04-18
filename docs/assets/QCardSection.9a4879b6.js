db8aba0d2e1deeaf0af7460dc5121eddddd4aeb3
PWZeil <p.w.zeilstra@saxion.nl>
2025-04-18 10:18:55 +0200

    First push with build

---
+++ docs/assets/QCardSection.9a4879b6.js
@@ -0,0 +1,1 @@
+import{n as e,c as r,h as n,A as s}from"./index.a574533b.js";var i=e({name:"QCardSection",props:{tag:{type:String,default:"div"},horizontal:Boolean},setup(t,{slots:a}){const o=r(()=>`q-card__section q-card__section--${t.horizontal===!0?"horiz row no-wrap":"vert"}`);return()=>n(t.tag,{class:o.value},s(a.default))}});export{i as Q};
---
+++ docs/assets/use-quasar.c4351920.js
@@ -0,0 +1,1 @@
+import{i as a,az as r}from"./index.a574533b.js";function u(){return a(r)}export{u};
